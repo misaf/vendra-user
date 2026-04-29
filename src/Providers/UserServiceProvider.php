ea860c666bce78e071320eae102b4d3fc102c415
Misaf <misaf.1990@gmail.com>
2026-04-29 05:04:33 +0330

    closed #5: Require All Packages to Use spatie/laravel-package-tools as Foundation

--- src/Providers/UserServiceProvider.php
+++ src/Providers/UserServiceProvider.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Misaf\VendraUser;
+namespace Misaf\VendraUser\Providers;
 
 use Filament\Panel;
 use Illuminate\Contracts\Foundation\Application;
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Gate;
 use Misaf\VendraUser\Models\User;
 use Misaf\VendraUser\Services\UserService;
+use Misaf\VendraUser\UserPlugin;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -23,12 +24,9 @@ final class UserServiceProvider extends PackageServiceProvider
             ->name('vendra-user')
             ->hasConfigFile()
             ->hasTranslations()
-            // ->hasMigrations([
-            //     'add_tenant_id_column_to_roles_table',
-            //     'add_tenant_id_column_to_permissions_table',
-            //     'add_description_column_to_roles_table',
-            //     'add_description_column_to_permissions_table',
-            // ])
+            ->hasMigrations([
+                'create_users_table',
+            ])
             ->hasInstallCommand(function (InstallCommand $command): void {
                 $command->askToStarRepoOnGitHub('misaf/vendra-user');
             });
@@ -36,6 +34,8 @@ final class UserServiceProvider extends PackageServiceProvider
 
     public function packageRegistered(): void
     {
+        $this->app->bind('user-service', fn(Application $app) => new UserService());
+
         Panel::configureUsing(function (Panel $panel): void {
             if ('admin' !== $panel->getId()) {
                 return;
@@ -43,8 +43,6 @@ final class UserServiceProvider extends PackageServiceProvider
 
             $panel->plugin(UserPlugin::make());
         });
-
-        $this->app->bind('user-service', fn(Application $app) => new UserService());
     }
 
     public function packageBooted(): void
