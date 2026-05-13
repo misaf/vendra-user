ac72e3dd9dd7881c458f5f386c9e04b40f4a2962
Misaf <misaf.1990@gmail.com>
2026-05-13 13:09:45 +0330

    feat: add German translations

---
+++ resources/lang/de/attributes.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'username'   => 'Benutzername',
+    'rating'     => 'Bewertung',
+    'created_at' => 'Erstellt am',
+    'updated_at' => 'Aktualisiert am',
+    'deleted_at' => 'Gelöscht am',
+];
---
+++ resources/lang/de/widgets.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'latest_transaction_table'                 => 'Neueste Transaktion',
+    'latest_transaction_table_description'     => 'Beschreibung der neuesten Transaktion',
+    'deposit_transaction_stats'                => 'Einzahlungstransaktion',
+    'deposit_transaction_stats_description'    => 'Statistiken zu Einzahlungstransaktionen',
+    'withdrawal_transaction_stats'             => 'Auszahlungstransaktion',
+    'withdrawal_transaction_stats_description' => 'Statistiken zu Auszahlungstransaktionen',
+    'commission_transaction_stats'             => 'Provisionstransaktion',
+    'commission_transaction_stats_description' => 'Statistiken zu Provisionstransaktionen',
+    'bonus_transaction_stats'                  => 'Bonus-Transaktion',
+    'bonus_transaction_stats_description'      => 'Statistiken zu Bonus-Transaktionen',
+    'transfer_transaction_stats'               => 'Übertragungstransaktion',
+    'transfer_transaction_stats_description'   => 'Statistiken zu Übertragungstransaktionen',
+];
