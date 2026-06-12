f965f152cdb1fd1d676af99f6efeeca12eff4757
Misaf <misaf.1990@gmail.com>
2026-06-12 19:55:01 +0330

    feat: add CreateUserAction

---
+++ src/Actions/CreateUserAction.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Misaf\VendraUser\Actions;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Misaf\VendraTenant\Models\Tenant;
+use Misaf\VendraUser\Models\User;
+
+final class CreateUserAction
+{
+    public function execute(
+        Tenant $tenant,
+        string $username,
+        string $email,
+        string $password,
+        bool $isVerified,
+    ): User {
+        $user = User::query()->create([
+            'tenant_id'         => $tenant->id,
+            'username'          => $username,
+            'email'             => $email,
+            'email_verified_at' => $isVerified ? Carbon::now() : null,
+            'password'          => Hash::make($password),
+        ]);
+
+        $user->tenants()->syncWithoutDetaching([$tenant->id]);
+
+        return $user;
+    }
+}
