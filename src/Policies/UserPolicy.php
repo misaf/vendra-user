94d6cd4d760e19a815c7744297ad033675c40339
Misaf <misaf.1990@gmail.com>
2026-07-06 13:06:35 +0330

    Clean up module autoload and policies

--- src/Policies/UserPolicy.php
+++ src/Policies/UserPolicy.php
@@ -5,65 +5,65 @@ declare(strict_types=1);
 namespace Misaf\VendraUser\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Misaf\VendraUser\Enums\UserPolicyEnum;
-use Misaf\VendraUser\Models\User;
 
 final class UserPolicy
 {
     use HandlesAuthorization;
 
-    public function create(User $user): bool
+    public function create(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::CREATE);
+        return $user->can(UserPolicyEnum::CREATE->value);
     }
 
-    public function delete(User $user): bool
+    public function delete(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::DELETE);
+        return $user->can(UserPolicyEnum::DELETE->value);
     }
 
-    public function deleteAny(User $user): bool
+    public function deleteAny(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::DELETE_ANY);
+        return $user->can(UserPolicyEnum::DELETE_ANY->value);
     }
 
-    public function forceDelete(User $user): bool
+    public function forceDelete(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::FORCE_DELETE);
+        return $user->can(UserPolicyEnum::FORCE_DELETE->value);
     }
 
-    public function forceDeleteAny(User $user): bool
+    public function forceDeleteAny(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::FORCE_DELETE_ANY);
+        return $user->can(UserPolicyEnum::FORCE_DELETE_ANY->value);
     }
 
-    public function replicate(User $user): bool
+    public function replicate(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::REPLICATE);
+        return $user->can(UserPolicyEnum::REPLICATE->value);
     }
 
-    public function restore(User $user): bool
+    public function restore(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::RESTORE);
+        return $user->can(UserPolicyEnum::RESTORE->value);
     }
 
-    public function restoreAny(User $user): bool
+    public function restoreAny(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::RESTORE_ANY);
+        return $user->can(UserPolicyEnum::RESTORE_ANY->value);
     }
 
-    public function update(User $user): bool
+    public function update(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::UPDATE);
+        return $user->can(UserPolicyEnum::UPDATE->value);
     }
 
-    public function view(User $user): bool
+    public function view(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::VIEW);
+        return $user->can(UserPolicyEnum::VIEW->value);
     }
 
-    public function viewAny(User $user): bool
+    public function viewAny(Authorizable $user): bool
     {
-        return $user->can(UserPolicyEnum::VIEW_ANY);
+        return $user->can(UserPolicyEnum::VIEW_ANY->value);
     }
 }
