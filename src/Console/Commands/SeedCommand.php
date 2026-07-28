b8e5ffac3728e89071f100d5979360296d334840
misaf <misaf.1990@gmail.com>
2026-07-28 11:39:39 +0000

    docs: add UPGRADING guide and update README files across packages

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -14,10 +14,10 @@ final class SeedCommand extends TenantSeedCommand
     protected const string MODULE_NAME = UserPlugin::ID;
 
     protected $signature = self::MODULE_NAME . ':seed
-        {tenant : Tenant ID or slug to seed blog data for}
+        {tenant : Tenant ID or slug to seed user data for}
         {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies, demo-contents}';
 
-    protected $description = 'Seed blog module data for a tenant';
+    protected $description = 'Seed user module data for a tenant';
 
     /**
      * @return array<string, class-string>
