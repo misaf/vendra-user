85f6a71c5ae0c8489872a846809dd37195ce389b
misaf <misaf.1990@gmail.com>
2026-08-29 22:00:10 +0000

    Merge remote-tracking branch 'origin/1.x' into 1.x

--- src/Filament/Clusters/Resources/Users/Schemas/UserForm.php
+++ src/Filament/Clusters/Resources/Users/Schemas/UserForm.php
@@ -14,7 +14,7 @@ use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\Rules\Unique;
 use Livewire\Component as Livewire;
 
-use Misaf\LaravelEmailValidation\Rules\EmailValidation;
+use Misaf\LaravelEmailVerification\Rules\EmailValidation;
 use Misaf\VendraSupport\Capabilities\TagIntegration;
 use Misaf\VendraSupport\Filament\Actions\GeneratePasswordAction;
 use Misaf\VendraSupport\Tenancy\TenantAwareness;
